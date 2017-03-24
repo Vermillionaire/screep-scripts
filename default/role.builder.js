124087563d6c45e45099bc7176274994a44dbb5a
Your Name <you@example.com>
2017-03-23 23:47:07 -0400

    readded a repair function

--- default/role.builder.js
+++ default/role.builder.js
@@ -26,28 +26,21 @@ var roleBuilder = {
         if (targets.length > 0) {
           creep.memory.target = targets[0].id;
           creep.memory.path = creep.pos.findPathTo(targets[0]);
-        }
-        /*
-        else {
+        } else {
           var targets = creep.room.find(FIND_STRUCTURES);
           if (targets.length) {
             var i = 0;
             for (var i = 0; i < targets.length; i++) {
               if (targets[i].hitsMax > targets[i].hits && targets[i].hits <
                 3000) {
-                i++;
-                console.log(i);
-                if (creep.repair(targets[i]) == ERR_NOT_IN_RANGE) {
-                  creep.moveTo(targets[i], {
-                    visualizePathStyle: {
-                      stroke: '#ffffff'
-                    }
-                  });
-                }
+                //i++;
+                creep.memory.target = targets[i].id;
+                creep.memory.path = creep.pos.findPathTo(targets[i]);
+                //console.log(i);
               }
             }
           }
-        } */
+        }
       }
 
       var target = Game.getObjectById(creep.memory.target);
@@ -59,12 +52,20 @@ var roleBuilder = {
       }
 
       //if (targets.length > 0) {
-      console.log(target);
-      if (creep.build(target) ==
-        ERR_NOT_IN_RANGE) {
-        //console.log(targets.length);
-        console.log(creep.moveByPath(creep.memory.path));
+      if (!(target instanceof ConstructionSite)) {
+        console.log("Repairing: " + target);
+        if (creep.repair(target) == ERR_NOT_IN_RANGE) {
+          var code = creep.moveByPath(creep.memory.path);
+          console.log(code);
+        }
+      } else {
+        if (creep.build(target) == ERR_NOT_IN_RANGE) {
+          //console.log(targets.length);
+          console.log("Constructing: " + target);
+          var code = creep.moveByPath(creep.memory.path);
+        }
       }
+
       //}
 
     } else {
@@ -96,7 +97,13 @@ var roleBuilder = {
 
       console.log(target);
       if (creep.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-        console.log(creep.moveByPath(creep.memory.path));
+        var code = creep.moveByPath(creep.memory.path);
+        console.log(code);
+        if (code == ERR_NOT_FOUND) {
+          console.log("Creep path error");
+          delete creep.memory.path;
+          delete creep.memory.target;
+        }
       }
 
     }
